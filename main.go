c45841d3bfc322841d972a6cada5971bec7342a1
SegFault42 <SegFault42@protonmail.com>
2019-12-25 19:14:12 +0100

    fg

--- main.go
+++ main.go
@@ -40,17 +40,11 @@ func getDegree(listLeft []string, listRight []string) int {
 		}
 	}
 
-	// TODO : fix crash here
 	for _, elem := range listRight {
 		tmp, _ := strconv.Atoi(string(elem[len(elem)-1]))
 		if tmp > degree {
 			degree = tmp
 		}
-		// addition chaque terme
-		//add := strings.Split(elem, "*")
-		//fmt.Println(add)
-		//number, _ := strconv.Atoi(add[0])
-		//fmt.Println(number)
 	}
 
 	return (degree)
@@ -84,24 +78,67 @@ func cleanEquation(equation []string) ([]string, []string) {
 	return listLeft, listRight
 }
 
-func formatEquation(left []string, right []string) string {
-	regOnlyDigit := regexp.MustCompile(`[-+]?[0-9]*\.?[0-9]`)
+// this function add missing character in each term
+func formatEquation(array []string) []string {
+	regOnlyDigit := regexp.MustCompile(`^[-+]?[0-9]*\.?[0-9]*$`)
+	regOnlyX := regexp.MustCompile(`^[-+]?[X]$`)
+	regNoPow := regexp.MustCompile(`^[-+]?[0-9]*\.?[0-9]\*X*$`)
+	regNoCoef := regexp.MustCompile(`^[+-]?X\^[0-9]$`)
 
-	for i, elem := range left {
-		if regOnlyDigit.Match([]byte(elem)) == true {
-			left[i] = elem + "*X^0"
+	for i, elem := range array {
+		if regOnlyDigit.MatchString(elem) == true {
+			array[i] = elem + "*X^0"
+		} else if regOnlyX.MatchString(elem) == true {
+			if elem[0] == '-' {
+				array[i] = "-1*X^1"
+			} else {
+				array[i] = "+1*X^1"
+			}
+		} else if regNoPow.MatchString(elem) == true {
+			array[i] = elem + "^1"
+		} else if regNoCoef.MatchString(elem) == true {
+			if elem[0] == '-' {
+				elem = elem[1:len(elem)]
+				array[i] = "-1*" + elem
+			} else if elem[0] == '+' {
+				elem = elem[1:len(elem)]
+				array[i] = "+1*" + elem
+			} else if elem[0] == 'X' {
+				elem = elem[1:len(elem)]
+				array[i] = "+1*X" + elem
+			}
 		}
 	}
 
-	//pretty.Println(left)
-	return ""
+	return (array)
+}
+
+func moveLeftToRight(left []string, right []string) []string {
+	for i, elem := range right {
+		if strings.Contains(right[i], "+") == true {
+			right[i] = strings.Replace(elem, "+", "-", 1)
+		}
+	}
+
+	pretty.Println("right = ", right)
+
+	return (left)
 }
 
 func ReduceForm(equation []string) []string {
 	left, right := cleanEquation(equation)
 
+	left = formatEquation(left)
+	right = formatEquation(right)
+
 	pretty.Println(left)
-	formatEquation(left, right)
+	// move all right side to left side
+	left = moveLeftToRight(left, right)
+
+	//sumTerme(left)
+
+	pretty.Println(left)
+	pretty.Println(right)
 
 	//if len(left) > 3 || len(right) > 3 {
 	//log.Printf("Equation bad formatted\n")
--- main_test.go
+++ main_test.go
@@ -1,30 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
+
+	"github.com/kr/pretty"
 )
 
-type test struct {
-	input    []string
-	expected bool
-}
-
 func TestReducedForm(t *testing.T) {
-	var tests = []test{
-		{{"-5 * X^0"}, true},
-		{{"+4 * X^1"}, true},
-		{{"+90"}, false},
+	var tests = []string{
+		"-5 * X^0",
+		"+4 * X^1",
+		"+90",
 	}
 
 	reg := regexp.MustCompile(`[-+]?[0-9]*\.?[0-9]*\*X[\^][0-2]`)
 
-	for _, elem := range tests {
-		reduced := ReduceForm(elem.input)
-		for _, item := range reduced {
-			if reg.Match([]byte(item)) == false {
-				t.Error("Equation Bad formated")
-			}
+	reduced := ReduceForm(tests)
+	pretty.Println("red =", reduced)
+	for _, item := range reduced {
+		fmt.Println(item)
+		if reg.Match([]byte(item)) == false {
+			t.Error("Equation Bad formated")
 		}
 	}
 }
